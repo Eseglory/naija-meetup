995aa77443361e3ce3fd02af03cfe813814e06df
Glory Eseosa <eseglory@hotmail.com>
2024-04-13 19:22:18 +0100

    resolve pydentic issues

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from .routes import users, auth, password_reset, post_content
+import uvicorn
+from os import getenv
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(password_reset.router)
 app.include_router(post_content.router)
+
+if __name__ == "__main__":
+    port = int(getenv("PORT, 8000"))
+    uvicorn.run("api.main:app", host="0.0.0.0", port=port, reload=True)
