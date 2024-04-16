ce877aea1cee47771c6089dbf593daf8557cfba6
Glory Eseosa <eseglory@hotmail.com>
2024-04-16 16:48:19 +0100

    add user details

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -27,14 +27,26 @@ async def login(user_credentials: LoginRequest):
     if not user_credentials.username or not user_credentials.password:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username and password are required")
 
-    user = await utils.db["users"].find_one({"username": user_credentials.username})
+    user = await utils.db["users"].find_one({"username": user_credentials.username, "is_active": True})
     if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
     if user["is_email_verified"] == False:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You have not completed your registration process")
 
     if user and utils.verify_password(user_credentials.password, user["password"]):
-        access_token = create_access_token({"id": user["_id"]})
-        return {"access_token": access_token, "token_type": "bearer"}
+        user_id_str = str(user["_id"])  # Convert ObjectId to string
+        access_token = create_access_token({"id": user_id_str})
+        return {
+            "access_token": access_token,
+              "token_type": "bearer",
+              "user_details": {
+                  "email": user["email"],
+                  "username": user["username"],
+                  "first_name": user["first_name"],
+                  "last_name": user["last_name"],
+                  "registration_date": user["creation_date"] 
+              }
+                }
     else:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid user credentials")
